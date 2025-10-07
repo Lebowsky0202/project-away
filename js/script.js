9d53d94700a845c5ac1be2ca0df3d9b33a91a35d
Kuat <example@gmail.com>
2025-10-07 23:02:46 +0500

    Первая практика

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 'use strict'
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '')
+
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false,
+}
+
+const lastViewedMovie = prompt('Один из последних просмотренных фильмов?', '')
+const assessmentMovie = prompt('На сколько оцените его', '')
+const lastViewedMovie2 = prompt('Один из последних просмотренных фильмов?', '')
+const assessmentMovie2 = prompt('На сколько оцените его', '')
+
+personalMovieDB.movies[lastViewedMovie] = assessmentMovie
+personalMovieDB.movies[lastViewedMovie2] = assessmentMovie2
+
+console.log(personalMovieDB)
