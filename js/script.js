f16010883b84c8cd138ddead1a6a9fc7a90e82e7
Kuat <example@gmail.com>
2025-10-12 10:44:23 +0500

    работа над программой по улутшению фильмов

--- js/script.js
+++ js/script.js
@@ -7,14 +7,26 @@ const personalMovieDB = {
 	movies: {},
 	actors: {},
 	genres: [],
-	privat: false,
+	private: false,
 }
 
 for (let i = 0; i < 2; i++) {
-	const lastViewedMovie = prompt('Один из последних просмотренных фильмов?', '')
-	const assessmentMovie = prompt('На сколько оцените его', '')
+	const a = prompt('Один из последних просмотренных фильмов?', '')
+	const b = prompt('На сколько оцените его', '')
 
-	personalMovieDB.movies[lastViewedMovie] = assessmentMovie
+	if (a != '' && b != '' && a != null && b != null && a.length < 50) {
+		personalMovieDB.movies[a] = b
+		console.log('done')
+	} else {
+		console.log('error')
+		i--
+	}
 }
+
+if (personalMovieDB.count < 10) console.log('Просмотренно мало фильмов')
+else if (personalMovieDB.count > 10 && personalMovieDB.count < 30)
+	console.log('Вы классический зритель')
+else if (personalMovieDB.count > 30) console.log('Вы киноман')
+else console.log('Error')
 
 console.log(personalMovieDB)
